dda15c40b6f1436f8753dd908316ffb74e1e94b1
Violet Death <ladymariaownsbro@gmail.com>
2020-07-10 15:39:07 -0500

    work-a

---
+++ models/workout.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+//   name: {
+//     type: String,
+//     trim: true,
+//     required: "Enter a name for transaction"
+//   },
+//   value: {
+//     type: Number,
+//     required: "Enter an amount"
+//   },
+//   date: {
+//     type: Date,
+//     default: Date.now
+//   }
+day: {
+    type: Date,
+    default: Date.now
+},
+exercises: [
+    {
+        type: {
+            type:String,
+            trim: true,
+            required: "Enter an exercise type"
+        },
+        name: {
+            type:String,
+            trim: true,
+            required: "Enter an exercise name"
+        },
+        duration: {
+            type:Number,
+            required: "Enter a duration time"
+        },
+        weight: {
+            type:Number
+        },
+        reps: {
+            type:Number
+        },
+        sets: {
+            type:Number
+        },
+        distance: {
+            type:Number
+        }
+    }
+]
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
